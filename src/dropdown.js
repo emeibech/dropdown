3786343fe01e38d0de7b7d4a535cfb546814b3cf
emeibech <emeibech@gmail.com>
2022-12-10 18:39:30 +0800

    Refactor html, css, and javascript

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,12 +1,10 @@
 export const dropdown = () => {
     document.addEventListener('click', (element) => {
-
         const dropdownNodes = document.querySelectorAll('[data-dropdown]');
-        const currentDropdown = element.target.lastElementChild;
+        const currentDropdown = element.target.nextElementSibling;
 
         if (element.target.matches('[data-dropdown-button]')) {
             if (currentDropdown.classList.contains('active')) {
-                console.log('active');
                 currentDropdown.classList.toggle('active');
             } else {
                 dropdownNodes.forEach((item) => {
